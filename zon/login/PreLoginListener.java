5688c4c6923a9032263c67b371176dc7192fa4a4
dormao <452635801@qq.com>
2017-06-16 00:11:44 +0800

    PreLogin的注意事项

--- zon/login/PreLoginListener.java
+++ zon/login/PreLoginListener.java
@@ -4,6 +4,12 @@ import cn.nukkit.Player;
 
 /**
  * Zon Project
+ *
+ * 需要实现方法 : onPreLogin
+ * 注意!返回值很重要!!
+ * 如果返回true，那么登录器将正常把玩家送入登录区进行验证!
+ * 如果返回false, 那么登录器将会放弃这个玩家的验证，这个玩家只能由其他插件处理!
+ * 实现时请谨慎使用返回值
  */
  
 public interface PreLoginListener {
